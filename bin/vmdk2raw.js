05867f411f0d9cd3d5bca68b7f8d16b49b3a0f57
Orlando Vazquez <ovazquez@gmail.com>
2011-12-01 12:28:05 -0800

    Organize the repo a little bit.

---
+++ bin/vmdk2raw.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+var async = require('async');
+var VMDK = require('../lib/vmdk');
+
+if (!process.argv[2]) {
+  console.error("Must pass in vmdk file as argument");  
+  process.exit(1);
+}
+
+var filename = process.argv[2];
+var v = new VMDK({ filename: filename });
+
+v.open(function (error) {
+  var stream = v.stream();
+  stream.pipe(process.stdout, { end: false });
+  stream.start();
+});
